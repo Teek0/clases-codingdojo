d5e79d957816bccb8e102f7fa00f0433c97a9284
Nicole Sáez <86169717+Teek0@users.noreply.github.com>
2023-05-24 17:51:17 -0400

    dia 7

---
+++ Segunda semana/Día 6/fundamentos.js
@@ -0,0 +1,21 @@
+/*Esto
+es
+un
+comentario
+en bloque*/
+//Esto es un comentario en línea
+let edad=25;                //variable con tipo de dato "number"
+let nombre="Alejandro";     //variable con tipo de dato "string"
+let mayorDeEdad=true;       //variable con tipo de dato "boolean"
+let calificacion=null;      //variable con tipo de dato "null"
+let apellido;               //variable con tipo de dato "undefine"
+
+console.log( nombre, edad );
+
+let resultado = 4 + 5 * 2;
+console.log( resultado );
+resultado = ( 4 + 5 ) * 2;
+console.log( resultado );
+
+let resultado2 = nombre + edad;
+console.log( resultado2 );
