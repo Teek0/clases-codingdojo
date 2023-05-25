c4f3d597e87320b0e2f0b7b9d256d6137e3c4b13
Nicole Sáez <86169717+Teek0@users.noreply.github.com>
2023-05-25 19:32:58 -0400

    dojonary v2

--- Ejercicios-de-la-plataforma/3 JavaScript/Evento onclick/js/index.js
+++ Ejercicios-de-la-plataforma/3 JavaScript/Evento onclick/js/index.js
@@ -1,12 +1,33 @@
 function logout(button){
     button.innerText="Logout";
 }
-function addDefDel(button){
-    button.remove();
+function addDefinition(button){
+    let contenedor=document.querySelector(".words");
+    contenedor.innerHTML+=
+            `<article class="tarjeta">
+                <div class="palabra">
+                    <h2>Ninja</h2>
+                    <h3 class="italic">noun</h3>
+                    <p>Plural: <span class="italic">ninjas</span></p>
+                    <button onclick="likeAlert(this)">0 likes</button>
+                </div>
+                <p>A practitioner skilled in the <span class="linked">Japanese</span> art of <span class="linked">ninjutsu</span></p>
+            </article>`;
+
+    
 }
 function likeAlert(elemento){
     alert("Ninja was liked");
     let num = parseInt(elemento.innerHTML);
     num++;
     elemento.innerHTML= num+" likes";
+}
+function deleted(elemento){
+    elemento.closest(".interiorAside").remove();
+}
+function hideThis(elemento){
+    elemento.closest(".interiorAside").classList.add("hide");
+}
+function showThis(){
+    document.getElementById("boxyBox").classList.remove("hide");
 }
