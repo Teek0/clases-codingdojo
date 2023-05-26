db599188f3fca0ec9543495b51f55264e275e53e
Nicole Sáez <86169717+Teek0@users.noreply.github.com>
2023-05-25 22:13:00 -0400

    video task completed

---
+++ Ejercicios-de-la-plataforma/3 JavaScript/Vista previa de video/script.js
@@ -0,0 +1,21 @@
+console.log("page loaded...");
+
+function startPreview(video){
+    video.muted=true;
+    video.load();
+    video.play();
+}
+function stopPreview(video){
+    video.pause();
+}
+
+
+function activeControls(video) {
+    video.load();
+    video.setAttribute('controls', '');
+    video.muted=false;
+    video.play();
+    video.removeAttribute('onmouseover');
+    video.removeAttribute('onmouseout');
+    video.removeAttribute('onclick');
+}
