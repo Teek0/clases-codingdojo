22d4c27bc66361c337b1194dba27c476a271a010
Nicole Sáez <86169717+Teek0@users.noreply.github.com>
2023-05-26 11:58:27 -0400

    clase 9

--- Ejercicios-de-la-plataforma/3 JavaScript/Evento onclick/js/index.js
+++ Ejercicios-de-la-plataforma/3 JavaScript/Evento onclick/js/index.js
@@ -1,11 +1,23 @@
 function logout(button){
     if (button.innerHTML=="Login") {
+
+        alert("Has deslogeado exitosamente");
         button.innerText="Logout";
     }
     else {
         button.innerText="Login";
+        alert("Bienvenido de vuelta :)");
     }
 }
+
+function search(){
+    setTimeout(alertaDeBusqueda,2000); //asincrono
+}
+function alertaDeBusqueda(){
+    let mensaje=document.querySelector(".search");
+    alert("Tu busqueda es la siguiente: " + mensaje.value);
+}
+
 function addDefinition(){
     let contenedor=document.querySelector(".words");
     contenedor.innerHTML+=
@@ -20,7 +32,7 @@ function addDefinition(){
             </article>`;
 }
 function likeAlert(elemento){
-    alert("Ninja was liked");
+    //alert("Ninja was liked");
     let num = parseInt(elemento.innerHTML);
     num++;
     elemento.innerHTML= num + " likes";
@@ -34,11 +46,31 @@ function hideThis(elemento){
 function showThis(){
     document.getElementById("boxyBox").classList.remove("hide");
 }
+/*---------- SELECT PAIS --------------*/
+var pais=document.querySelector('#bandera');
 
 function seleccionarPais(elemento){
-    alert(elemento.value);
+    if(elemento.value=="CH")
+    {pais.src="img/chile.png";}
+    else if(elemento.value=="MX")
+    {pais.src="img/mexico.png";}
+    else if(elemento.value=="PE")
+    {pais.src="img/peru.png";}
+    else if(elemento.value=="US")
+    {pais.src="img/USA.png";}
+    document.getElementById("main").classList.add("opacidad");
+    document.getElementById("header").classList.add("opacidad");
+    document.getElementById("modal").classList.remove("hide");
 }
+
+function hideModal(elemento){
+    document.getElementById("main").classList.remove("opacidad");
+    document.getElementById("header").classList.remove("opacidad");
+    elemento.closest("#modal").classList.add("hide");
+}
+/*---------- END SELECT PAIS --------------*/
 
 function desplegarTexto(element){
     console.log(element.value);
 }
+
--- Ejercicios-de-la-plataforma/3 JavaScript/cat vs dog/js/script.js
+++ Ejercicios-de-la-plataforma/3 JavaScript/cat vs dog/js/script.js
@@ -1,9 +1,9 @@
 var animalImg=document.querySelector('#fav-animal');
 console.log(animalImg.src);
 
-function pickCat(){
+function pickCat(element){
     animalImg.src="img/gato.jpeg";
 }
-function pickDog(){
+function pickDog(element){
     animalImg.src="img/perro.jpeg";
 }
